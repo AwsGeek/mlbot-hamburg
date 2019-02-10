e8cbcfc98f7fd33c66265f089c5c070f8708a795
awsgeek <ec2-user@ip-10-0-0-153.us-west-2.compute.internal>
2019-02-10 18:00:37 +0000

    Cloudformation

---
+++ build/Dispatcher/handler.py
@@ -0,0 +1,37 @@
+import os
+import json
+import boto3
+
+session = boto3.session.Session()
+sqs = session.client('sqs')
+
+def response(code, body):
+    return {
+        'statusCode': str(code),
+        'body': body,
+        'headers': { 'Content-Type': 'application/json'}
+    }
+ 
+def success(res=None):
+    return response(200, json.dumps(res))
+ 
+def failure(err):
+    return response(400, err.message)
+ 
+def lambda_handler(event, context):
+ 
+    print(event)
+    
+    params = json.loads(event['body'])
+    if params['type'] == "url_verification":
+        return success(  {'challenge': params['challenge']} )
+
+    if params['type'] == "event_callback":
+
+        response = sqs.send_message(
+            QueueUrl=os.environ['QueueUrl'],
+            MessageBody=(event['body'])
+        )
+        return success()        
+        
+    return failure(Exception('Invalid event type: %s' % (params['type'])))
---
+++ build/Handler/handler.py
@@ -0,0 +1,54 @@
+import os
+import re
+import json
+import boto3
+from botocore.vendored import requests
+
+lam = boto3.client('lambda')
+
+def classify_aircraft(url):
+
+    aircraft = "None";    
+    result = lam.invoke(
+        FunctionName=os.environ['DetectorName'],
+        InvocationType='RequestResponse',
+        Payload=json.dumps({ "url": url })
+    )
+    detected = json.loads(result['Payload'].read().decode('utf8'))
+
+    if len(detected) == 1 and detected[0]['score'] > 99:
+        result = lam.invoke(
+            FunctionName=os.environ['ClassifierName'],
+            InvocationType='RequestResponse',
+            Payload=json.dumps({ "url": url})
+        )
+        aircraft = json.loads(result['Payload'].read().decode('utf8'))
+        
+    return aircraft
+
+def lambda_handler(event, context):
+    print(event)
+    
+    for record in event['Records']:
+
+        request = json.loads(record['body'])
+        
+        event = request['event']
+        text = event['text']
+        
+        matches = re.search( 'classify\s+<(.*)>', text, re.IGNORECASE)
+        if matches:
+            
+            url = matches.group(1)
+            aircraft = classify_aircraft(url)
+            print(aircraft)
+
+            data = {'token':os.environ['SlackToken'], 
+                    'channel':event['channel'],
+                    'thread_ts':event['ts'],
+                    'text': "Aircraft detected: " + aircraft } 
+              
+            r = requests.post(url = 'https://slack.com/api/chat.postMessage', data = data)     
+
+
+
